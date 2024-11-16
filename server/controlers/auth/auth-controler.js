49107717e3b77ebaccfe7272af0bef7487525cf9
Code Pro <realheritagecompany@gmail.com>
2024-11-16 05:17:56 +0100

    Update auth-controler.js

--- server/controlers/auth/auth-controler.js
+++ server/controlers/auth/auth-controler.js
@@ -64,7 +64,7 @@ const loginUser = async (req, res) => {
       "CLIENT_SECRET_KEY",
       { expiresIn: "1d" }
     );
-    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+    res.status(200).json({
       success: true,
       message: "logged in sucessfully",
       token,
